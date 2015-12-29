36a145b67a97ee506c368f120f684f67745d086b
John Jones <johnjones4@gmail.com>
2015-12-28 22:52:52 -0500

    finished for now

--- index.js
+++ index.js
@@ -2,10 +2,26 @@ var Database = require('./lib/database');
 var BackupAgent = require('./lib/backupAgent');
 var GlacierTransmitter = require('./lib/transmitters/glacierTransmitter.js');
 var S3Transmitter = require('./lib/transmitters/s3Transmitter.js');
-var config = require('./config');
+var DummyTransmitter = require('./lib/transmitters/dummyTransmitter.js');
+
+var argv = require('minimist')(process.argv.slice(2),{
+  'default': {
+    'config': 'backuptool.json'
+  }
+});
+
+var config = require(argv.config);
 
 var database = new Database(config);
-var transmitter = new GlacierTransmitter(config);
+var transmitter;
+
+if (config.glacierVaultName) {
+  transmitter = new GlacierTransmitter(config);
+} else if (config.s3BucketName) {
+  transmitter = new S3Transmitter(config);
+} else {
+  transmitter = new DummyTransmitter();
+}
 
 database.connect(function(err) {
   if (err) {
@@ -14,4 +30,4 @@ database.connect(function(err) {
     var agent = new BackupAgent(config,database,console,transmitter);
     agent.execute();
   }
-})
+});
