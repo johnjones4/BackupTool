4c99595af1b7cf04176cf4886d99b6b4fb6f4e15
John Jones <johnjones4@gmail.com>
2015-12-29 19:49:16 -0500

    refactoring

--- index.js
+++ index.js
@@ -15,60 +15,78 @@ var argv = require('minimist')(process.argv.slice(2),{
 
 var config = require(argv.config);
 var database = new Database(config);
-var logger = {
-  'error': function() {},
-  'log': function() {},
-  'info': function() {}
-};
-
-if (argv.logging == 'error' || argv.logging == 'info') {
-  logger.error = console.error;
-}
-if (argv.logging == 'info') {
-  logger.log = console.log;
-  logger.info = console.info;
-}
+var logger = configLogger();
 
 database.connect(function(err) {
   if (err) {
     logger.error(err);
   } else {
     if (argv._.length == 1 && argv._[0] == 'backup') {
-      var fileQueue = new FileQueue(100);
-      var backuper;
-      var agent = new BackupAgent(config,database,logger,fileQueue);
-      agent.execute(function() {
-        if (config.glacierVaultName) {
-          backuper = new GlacierBackuper(config,database,logger,fileQueue);
-        } else if (config.s3BucketName) {
-          backuper = new S3Backuper(config,database,logger,fileQueue);
-        } else {
-          backuper = new DummyBackuper(config,database,logger,fileQueue);
-        }
-        backuper.execute(function() {
-          database.disconnect();
-          process.exit(0);
-        })
-      });
+      backup();
     } else if (argv._.length == 1 && argv._[0] == 'status') {
-      process.stdout.write('Waiting for status ...');
-      var interval = setInterval(function() {
-        database.getBackupStatus(function(err,part,total) {
-          if (err) {
-            logger.error(err);
-          } else if (total > 0) {
-            process.stdout.clearLine();
-            process.stdout.cursorTo(0);
-            process.stdout.write(Math.round((total - part) / total * 100) + '%');
-            if (part == 0) {
-              clearInterval(interval);
-              process.exit(0);
-            }
-          }
-        });
-      },500);
+      status();
     } else {
-      logger.error('Usage: backuptool [--config /path/to/file.json] <backup|status>')
+      usage();
     }
   }
 });
+
+function configLogger() {
+  var logger = {
+    'error': function() {},
+    'log': function() {},
+    'info': function() {}
+  };
+
+  if (argv.logging == 'error' || argv.logging == 'info') {
+    logger.error = console.error;
+  }
+  if (argv.logging == 'info') {
+    logger.log = console.log;
+    logger.info = console.info;
+  }
+
+  return logger;
+}
+
+function backup() {
+  var fileQueue = new FileQueue(100);
+  var backuper;
+  var agent = new BackupAgent(config,database,logger,fileQueue);
+  agent.execute(function() {
+    if (config.glacierVaultName) {
+      backuper = new GlacierBackuper(config,database,logger,fileQueue);
+    } else if (config.s3BucketName) {
+      backuper = new S3Backuper(config,database,logger,fileQueue);
+    } else {
+      backuper = new DummyBackuper(config,database,logger,fileQueue);
+    }
+    backuper.execute(function() {
+      database.disconnect();
+      process.exit(0);
+    })
+  });
+}
+
+function status() {
+  process.stdout.write('Waiting for status ...');
+  var interval = setInterval(function() {
+    database.getBackupStatus(function(err,part,total) {
+      if (err) {
+        logger.error(err);
+      } else if (total > 0) {
+        process.stdout.clearLine();
+        process.stdout.cursorTo(0);
+        process.stdout.write(Math.round((total - part) / total * 100) + '%');
+        if (part == 0) {
+          clearInterval(interval);
+          process.exit(0);
+        }
+      }
+    });
+  },500);
+}
+
+function usage() {
+  console.error('Usage: backuptool [--config /path/to/file.json] <backup|status>');
+}
